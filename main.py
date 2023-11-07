9d3c72d51712fef9403993962ee085b0f5749a3a
adef <basc11111@gmail.com>
2023-11-07 19:08:47 +0300

    fix

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ BROKER_PORT = int(os.environ.get("BROKER_PORT"))
 TOPIC = os.environ.get("TOPIC")
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    if not rc:
-        print(f'{datetime.utcnow()} | {client} connect to broker', flush=True)
-    else:
-        print(f'{datetime.utcnow()} | {client} not connect to broker {rc}', flush=True)
-
-
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     TOPIC = msg.topic
@@ -23,16 +15,12 @@ def on_message(client, userdata, msg):
     print(f'{datetime.utcnow()} | Get messege {TOPIC}: {payload}')
 
 
-def main():
-    client = mqtt.Client(CLIENT_NAME)
-    #client.on_connect = on_connect
+if __name__ == '__main__':
+    client = mqtt.Client(CLIENT_NAME, clean_session=True, userdata=None, transport="tcp")
+    # client.on_connect = on_connect
     client.on_message = on_message
 
     client.connect(host=BROKER_HOST, port=BROKER_PORT)
 
     client.subscribe(TOPIC)
     client.loop_forever()
-
-
-if __name__ == '__main__':
-    main()
