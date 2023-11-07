45bd41ebeeab3de33e562f36831d47cfdb7445de
adef <basc11111@gmail.com>
2023-11-07 19:26:23 +0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
@@ -8,6 +10,12 @@ BROKER_PORT = int(os.environ.get("BROKER_PORT"))
 TOPIC = os.environ.get("TOPIC")
 
 
+def on_connect(client, userdata, flags, rc):
+    if not rc:
+        print(f'{datetime.utcnow()} | {client} connect to broker', flush=True)
+    else:
+        print(f'{datetime.utcnow()} | {client} not connect to broker {rc}', flush=True)
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     TOPIC = msg.topic
@@ -15,12 +23,17 @@ def on_message(client, userdata, msg):
     print(f'{datetime.utcnow()} | Get messege {TOPIC}: {payload}')
 
 
-if __name__ == '__main__':
-    client = mqtt.Client(CLIENT_NAME, clean_session=True, userdata=None, transport="tcp")
-    # client.on_connect = on_connect
-    client.on_message = on_message
 
+
+
+def main():
+    client = mqtt.Client(CLIENT_NAME)
+    client.on_connect = on_connect
     client.connect(host=BROKER_HOST, port=BROKER_PORT)
-
     client.subscribe(TOPIC)
     client.loop_forever()
+
+
+
+if __name__ == '__main__':
+    main()
